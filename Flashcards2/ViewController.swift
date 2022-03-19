4ff12569a5121e86b3036e40583421e5bdca7672
elbbeele <95379572+elbbeele@users.noreply.github.com>
2022-03-18 21:28:06 -0700

    week 4 lab - adding new flashcards

--- Flashcards2/ViewController.swift
+++ Flashcards2/ViewController.swift
@@ -17,9 +17,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+    }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         frontLabel.isHidden = true
     }
+    
+    func updateFlashcard(question: String, answer: String){
+        frontLabel.text = question
+        backLabel.text = answer
+    }
+    
+    
 }
 
